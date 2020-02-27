39c3adca7dc3c00c58da356ebfd1df172209619b
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-02-27 16:59:14 +0800

    feat: 支持G6的封装

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Vuex from "vuex";
 
 import utils from "@xiyun/utils";
+import G6 from "@antv/g6";
 
 import qs from "qs";
 import axios from "axios";
@@ -20,3 +21,4 @@ export { lodash };
 export { qs };
 export { VueRouter };
 export { Vuex };
+export { G6 };
