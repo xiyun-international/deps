627c6f86a95b4685604f820043e3ac8e89c2754b
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-02-28 11:02:15 +0800

    feat: 支持nzh、BigNumber

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,37 @@ import copyToClipboard from "copy-to-clipboard";
 import lodash from "lodash";
 import moment from "moment";
 import PhotoViewer from "photoviewer";
+import BigNumber from "bignumber.js";
+import nzh from 'nzh';
+
 
 import injectComponents from "./components";
 import { message as AntdMessage } from "ant-design-vue";
+
+const Nzh = nzh.ch;
 
 injectComponents(Vue);
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
+// Vue
 export { Vue };
 export { VueRouter };
 export { Vuex };
 
+// 禧云
 export { utils };
-export { axios };
-export { copyToClipboard };
-export { lodash };
+export { AntdMessage };
+
+// 类库
 export { qs };
 export { G6 };
 export { moment };
+export { axios };
 export { PhotoViewer };
-export { AntdMessage };
+export { copyToClipboard };
+export { lodash };
+export { BigNumber };
+export { Nzh };
+
+
